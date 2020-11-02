5b695d4f82c5952427586ad530b11dbd556c0218
Марат Камалов <dead_island_hd@bk.ru>
2020-11-02 10:54:57 +0300

    lab2 main.py update

--- lab2/main.py
+++ lab2/main.py
@@ -3,6 +3,7 @@ from lab_python_oop.circle import Circle
 from lab_python_oop.square import Square
 import pyscreenshot as ImageGrab
 import numpy as np
+import cv2
 
 def main():
     r = Rectangle("синего", 7, 7)
@@ -11,6 +12,11 @@ def main():
     print(r)
     print(c)
     print(s)
-    screen = np.array(ImageGrab.grab(bbox=(10, 10, 20, 200)))
+    while True:
+     screen = np.array(ImageGrab.grab(bbox=(10, 10, 300, 300))) # метод внешнего пакета Pillow
+     cv2.imshow('window', screen)
+     if cv2.waitKey(25) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        break
 if __name__ == "__main__":
     main()
