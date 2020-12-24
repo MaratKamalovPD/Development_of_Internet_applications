9e0aaa0b08a7928de31dde2902dd1c2c9768effc
Марат Камалов <dead_island_hd@bk.ru>
2020-12-24 15:17:48 +0300

    lab8

---
+++ lab8/lab8/catalog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class Processor(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Description(models.Model):
+    processor_id = models.ForeignKey(Processor, on_delete=models.CASCADE)
+    description = models.CharField(max_length=200)
